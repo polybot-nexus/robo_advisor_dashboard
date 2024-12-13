6386dfddae732673302c9d745e1ad8a94fe686bd
Henry Chan <hyiprc@gmail.com>
2024-12-13 12:44:16 -0600

    style: fix linebreak, etc.

--- experiment_simulation.py
+++ experiment_simulation.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import time
+
+import pandas as pd
 
 df = pd.read_csv('datasets/oect_summary_posted_rf__plus_ml_combined_copy.csv')
 
@@ -11,12 +12,14 @@ remaining_rows = df[df['source'] != 'train']
 
 # Simulating adding a new line every minute (Here we reduce time for testing)
 for i, row in remaining_rows.iterrows():
-    new_dataset = pd.concat([new_dataset, pd.DataFrame([row])], ignore_index=True)
+    new_dataset = pd.concat(
+        [new_dataset, pd.DataFrame([row])], ignore_index=True
+    )
     print(f"Row {i} added to the new dataset.")
-    
+
     # Save the updated new_dataset to CSV every time a row is added
-    new_dataset.to_csv('datasets/oect_summary_posted_rf__plus_ml_combined.csv', index=False)
-    
+    new_dataset.to_csv(
+        'datasets/oect_summary_posted_rf__plus_ml_combined.csv', index=False
+    )
+
     time.sleep(10)
-
-
